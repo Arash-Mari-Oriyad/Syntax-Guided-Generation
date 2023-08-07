01b682d4b7c719a8e5d3ff3bb8a8c7e244a3cf42
Arash Mari Oriyad <arashmarioriyad@gamil.com>
2023-08-07 11:35:34 +0330

    update

--- syngen_diffusion_pipeline.py
+++ syngen_diffusion_pipeline.py
@@ -220,6 +220,7 @@ class SynGenDiffusionPipeline(StableDiffusionPipeline):
             prompt_embeds=prompt_embeds,
             negative_prompt_embeds=negative_prompt_embeds,
         )
+        print(prompt_embeds.shape)
 
         # 4. Prepare timesteps
         self.scheduler.set_timesteps(num_inference_steps, device=device)
